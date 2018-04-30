cf235bd297fb07c9063e20381861b4ccedf4914f
zmr0k <vadimklan@gmail.com>
2018-04-30 11:56:06 -0400

    nz_sandbox

--- strings_to_play.py
+++ strings_to_play.py
@@ -14,8 +14,9 @@ print "Source URL is:", url
 print "...is becoming upper: ", url.upper()
 print "URL has", len(url), "chars"
 
-#lets split by
+#lets split by followed char
 data = url.split("/")
+print "[data] massive is:", data
 
 print "Lenght is:", len(data) #3
 print data[0]  # mkyong.com
