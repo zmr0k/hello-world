121c4569df42eb156dff9bcd472bf8b1210dfa57
zmr0k <vadimklan@gmail.com>
2018-05-02 16:09:26 -0400

    play fun with ldap auth

--- scripts/zm_ldapAuth.py
+++ scripts/zm_ldapAuth.py
@@ -1,10 +1,36 @@
-import requests
+import requests, getpass
 
-url = "https://jiraops.corp-apps.com/"
-r = requests.get(url)
+jiraurl = "https://jiraops.corp-apps.com/"
+r = requests.get(jiraurl)
 
 if r.status_code == 200:
     print "Status code:", r.status_code
-    print "2Status code:", r.status_code
+# we dont want to get a page
+#    print "Text:", r.text
+    print "Header:", r.headers
+    print "Cookies", r.cookies
+
 else:
     print "Status code", r.status_code, "is not 200"
+    exit(-1)
+
+def jiraSession(jiraurl, username, password):
+  # Generate jira session cookies
+  url = '{}/rest/gadget/1.0/login'.format(jiraurl)
+  try:
+    req = requests.post(url, auth=(username, password))
+    try:
+      auth = req.cookies['atlassian.xsrf.token']
+    except:
+      print 'Jira authentication failed, please check your password'
+      exit(1)
+    return req.cookies
+  except requests.exceptions.RequestException as e:
+    print 'Error: {}\n{}'.format(e, req.text)
+    return
+
+username = "vklaniuk"
+password = getpass.getpass(prompt='Enter password for {}: '.format(username))
+
+myjirasession = jiraSession(jiraurl, username, password)
+print myjirasession
