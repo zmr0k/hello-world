9c68aa9794a06e08042eb5afbb34c52d58e79ca5
zmr0k <vadimklan@gmail.com>
2018-05-14 16:30:23 -0400

    lessons with writing to file...(adding file2/3)

--- scripts/zm_file_io.py
+++ scripts/zm_file_io.py
@@ -1,22 +1,13 @@
 
-# create new file
-with open("file2", "w+") as file2:
-    file2.write("Hello world 2\n")
-#    file2.readline()
-#    file2.write("Hello world 3")
-#    file2.write("Hello world 1\n")
-    file2.close()
+filename = "block_bruteforce_ips"
 
-#try:
-#    myfile = open("myfile.txt", "a+")
-#    myfile.seek(0)
-#    a = 8
-#    print "Reading 1st", a, "chars", myfile.read(a)
-#    myfile.seek(5)
-#    myfile.write("New string\n")
-#    print "Reading 2nd", a, "chars", myfile.read(a)
+try:
+    myfile = open(filename, "r")
+    myfile.seek(0)
+    print myfile.read()
+    myfile.seek(0)
+    print "reading line(s):\n"
+    print myfile.readlines(4)
 
-#    print myfile.readline()
-
-#finally:
-#    print "\nFile was closed correctly\n", myfile.close()
+finally:
+    print "\nFile was closed correctly\n", myfile.close()
