01f8344543f96bfcc0fc4149f54702909f5c832e
zmr0k <vadimklan@gmail.com>
2018-05-14 09:58:54 -0400

    lessons with writing to file...

--- scripts/zm_file_io.py
+++ scripts/zm_file_io.py
@@ -1,14 +1,22 @@
 
-try:
-    myfile = open("myfile.txt", "a+")
-    myfile.seek(0)
-    a = 8
-    print "Reading 1st", a, "chars", myfile.read(a)
-    myfile.seek(5)
-    addstring = myfile.write("New string\n")
-    print "Reading 2nd", a, "chars", myfile.read(a)
+# create new file
+with open("file2", "a+") as file2:
+    file2.write("Hello world 1\n")
+    file2.write("Hello world 2\n")
+#    file2.write("Hello world 3")
+#    file2.write("Hello world 1\n")
+    file2.close()
 
-    print myfile.readline()
+#try:
+#    myfile = open("myfile.txt", "a+")
+#    myfile.seek(0)
+#    a = 8
+#    print "Reading 1st", a, "chars", myfile.read(a)
+#    myfile.seek(5)
+#    myfile.write("New string\n")
+#    print "Reading 2nd", a, "chars", myfile.read(a)
 
-finally:
-    print "\nFile was closed correctly\n", myfile.close()
+#    print myfile.readline()
+
+#finally:
+#    print "\nFile was closed correctly\n", myfile.close()
