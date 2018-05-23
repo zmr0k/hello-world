b0639d2e044d25b36317eca805cfa548b1ce4fa8
zmr0k <vadimklan@gmail.com>
2018-05-23 18:21:05 -0400

    learning module JIRA.. auth and tried to create/update put comment on issue

--- scripts/zm_jira.py
+++ scripts/zm_jira.py
@@ -1,3 +1,24 @@
 from jira import JIRA
+import getpass
 
-jiraurl = JIRA("https://jiraops.mdev.corp-apps.com")
+jiralogin = "vklaniuk"
+jirapass = getpass.win_getpass()
+
+options = {
+    'server': "https://jiraops.mdev.corp-apps.com",
+#    'auth': "jiralogin", "jirapasss"
+
+}
+
+jira = JIRA(options, auth=(jiralogin, jirapass))
+
+
+print "Jira object is [", jira, "]"
+
+projects_list = jira.projects()
+print projects_list
+
+issue = jira.issue("ITO-98297")
+print issue
+
+update_issue = jira._check_update_()
