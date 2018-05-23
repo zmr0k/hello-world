6a9861aeeadbccba0cacf0359e6fc717b0a768e0
zmr0k <vadimklan@gmail.com>
2018-05-23 16:06:08 -0400

    converting to Cel from far. classics..

--- scripts/far_cel.py
+++ scripts/far_cel.py
@@ -1,6 +1,4 @@
-import math
 
 far = int(raw_input("Enter Far:"))
-print "User entered: [", far, "]"
 cel = (far - 32) * 5.0/9.0
-print "Celsium is:", cel
+print ("User entered:", far, "Celsium is:", cel)
