5e27c800495a66348422ad2e1baa3eba3f1bcd86
zmr0k <vadimklan@gmail.com>
2018-05-30 17:37:10 -0400

    learning Jira: search is important

--- scripts/zm_jira.py
+++ scripts/zm_jira.py
@@ -17,12 +17,12 @@ if __name__ == "__main__":
     except:
         print "\nJira auth failed. No session was created\n"
 
-#    try:
-#        projects_list = jira.projects()
-#        for i in projects_list:
-#            print i
-#    except:
-#        print "Error: Exception on projects list."
+    #try:
+    #    projects_list = jira.projects()
+    #    for i in projects_list:
+    #        print i
+    #except:
+    #    print "Error: Exception on projects list."
 
     myito = "ITO-98297"
     issue = jira.issue(myito)
@@ -37,15 +37,31 @@ if __name__ == "__main__":
         exit(-1)
 
     assignee = jira.assign_issue(myito, jiralogin)
-    jira.close()
+    #jira.close()
 
     #print "Fileds:", issue.fields()
-    for issue_field_name in issue.raw['fields']:
-        print "Field:", issue_field_name, "Value:", issue.raw['fields']['issue_field_name']
+    #for issue_field_name in issue.raw['fields']:
+    #    print "Field name: [", issue_field_name, "] // Value:", issue.raw['fields'][issue_field_name]
 
-    #create_issue = jira.create_issue(project="INC", summary="Creating issue with python", description="Description: Creating issue with python")
+
+    #print "Should be fields:", issue.raw['fields']
+    #['issuetype']
+
+    issue_dict = {
+        'project': {'name': 'IT Operations'},
+        'summary': 'zm-Savage-Test',
+        'description': 'zm_savage-test',
+        'issuetype': {'name': 'ITO-Support'},
+        #'customfield_11107': 'Not Applicable'
+    }
+
+    #try:
+    create_issue = jira.create_issue(fields=issue_dict)
+    #except:
+    #    print "Exception: issue wasn't created"
 #    jira.assign_issue(jiralogin)
 #    jira.close()
 
     #issues_in_inc = jira.search_issues('project=INC')
     #print issues_in_inc
+
