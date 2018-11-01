6eee1be371c1e7fd84f5bce13afb59a50ea43bc1
zmr0k <vadimklan@gmail.com>
2018-11-01 16:26:33 -0400

    Playing around with python

--- scripts/Hello_world.py
+++ scripts/Hello_world.py
@@ -17,7 +17,7 @@ for i in range(90, 100, 2):
 else:
 	print("Loop finished.")
 
-fruit = ["apple", "orange", "watermelon"]
+fruit = ["apple", "orange", "watermelon", "lemon"]
 adj = ["welloy", "big", "tasty"]
 
 for a in fruit:
@@ -25,3 +25,31 @@ for a in fruit:
 		print(b, a)
 else:
 	print("Finished.")
+
+for i in fruit:
+    if (i == "orange"):
+        continue
+    print("fruit is:", i)
+
+print(20 % 3, 18 / 3, 2**4)
+
+print((0.1+0.2)-0.3)
+print((0.1+0.2)-0.3)
+
+i = 2
+for k in range(3):
+    print(i**k)
+
+sum = 0.0
+for i in range(10):
+    sum += 0.1
+    print(sum)
+
+print(type(sum))
+
+my_string = "Vadim learns python"
+if (len(my_string) > 10):
+    print("Lenght:", my_string, "is", len(my_string))
+else: print("Lenght is <10", len(my_string))
+print(my_string[::-1])
+
